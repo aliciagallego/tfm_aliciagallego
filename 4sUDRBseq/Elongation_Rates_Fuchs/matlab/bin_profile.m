b8fbf77a7de2d66ee85ebd8320a8bd228bc76bd0
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-19 21:43:29 +0200

    Delete 4sUDRBseq directory

--- 4sUDRBseq/Elongation_Rates_Fuchs/matlab/bin_profile.m
+++
@@ -1,5 +0,0 @@
-function res = bin_profile (profile, bin_size)
-    number_of_bins = ceil(length(profile)/bin_size);
-    length_padding = mod(bin_size-mod(length(profile),bin_size),bin_size);
-    res = nanmean(reshape([profile' nan(1,length_padding)],bin_size,number_of_bins));
-end

3a12695cba1aae2e0628e3d6bd4376a9cb4a6bd2
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-19 23:32:38 +0200

    Update 2_generate_profiles.sh

--- 4sUDRBseq/Elongation_Rates_Fuchs/2_generate_profiles.sh
+++ 4sUDRBseq/Elongation_Rates_Fuchs/2_generate_profiles.sh
@@ -3,32 +3,25 @@
 process () {
 
 	cat $1 | grep -vP "^@" | awk -v OFS="\t" '($5>5 && $7=="=" && $9>0 && $9<800){print $3,$4,$4+$9}' | sort -k1,1 -k2,2n -k3,3n > REMOVE.ME.bed
-	
 	NUMS=$(seq 1 19); 
-
 	for NUM in $NUMS X Y
-
         do
                 cat REMOVE.ME.bed | grep -P "chr$NUM\t" > "chr"$NUM".bed"
                 cat /home/cc/JoseMiguel/Genome_files/mm10/chrom.sizes | grep -P "chr$NUM\t" > GENOME$NUM
-		#{printf ("%-8d\n", $3)} removes scientific notation for big numbers
+		#{printf ("%-20d\n", $3)} removes scientific notation for big numbers
                 genomeCoverageBed -d -i "chr"$NUM".bed" -g GENOME$NUM | awk '{printf ("%-20d\n", $3)}' | tr "\n" "\t" > profile_$NUM
                 rm -f GENOME$NUM
-		#rm -f REMOVE.ME.bed
         done
 }
 
 
 THISDIR=/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Elongation_rate_3/1.1_Rate_calculation
-
 for FILE in /media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/2_Sorting/Pull/Pull_sam/*.sam
 do
 	NAME=$(basename $FILE .sam)
 	mkdir $NAME
 	cd $NAME
-	
 	process $FILE &
-	
 	cd $THISDIR
 done
 wait

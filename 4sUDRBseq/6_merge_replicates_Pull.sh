41a053437eb1c608e9081550cc76f8fde6aac606
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-20 12:25:47 +0200

    Update 6_merge_replicates_Pull.sh

--- 4sUDRBseq/6_merge_replicates_Pull.sh
+++ 4sUDRBseq/6_merge_replicates_Pull.sh
@@ -1,10 +1,6 @@
-# comandos ejecutados en cadena usando este script desde el path:
-# cc@cc:/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/2_Sorting
+# path:/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/2_Sorting
 
 samtools merge Pull/WT0_MG9-11-13.bam   MG9-11.sorted.bam  MG9-13.sorted.bam 
 samtools merge Pull/WT5_MG9-12-14.bam   MG9-12.sorted.bam  MG9-14.sorted.bam
 samtools merge Pull/TKO0_MG9-15-17.bam  MG9-15.sorted.bam  MG9-17.sorted.bam 
 samtools merge Pull/TKO5_MG9-16-18.bam  MG9-16.sorted.bam  MG9-18.sorted.bam 
-
-
-

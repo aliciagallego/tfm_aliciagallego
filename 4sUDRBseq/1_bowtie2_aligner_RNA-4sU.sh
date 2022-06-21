e4364194f3ff5cfe48b5fe29b06f60c6a2784ddf
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-21 18:36:14 +0200

    Update 1_bowtie2_aligner_RNA-4sU.sh

--- 4sUDRBseq/1_bowtie2_aligner_RNA-4sU.sh
+++ 4sUDRBseq/1_bowtie2_aligner_RNA-4sU.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# bowtie2 alignment of fastq reads and mm10 indexed genome
+# -x basename of the index for the reference genome
+# -1 pair 1
+# -2 pair 2
+# -N number of mismatches to allowed in a seed alignment during multiseed alignment (0 or 1)
+
 NUMS=$(seq 11 18); 
 
 for NUM in $NUMS
 do
-	READS1="/media/cc/B/Josemi/TTseq_Feb2022/Reads_project_MG10/MG10_RNA_DRB-4sU/MG10_RNA/*S"$NUM"_R1_001.fastq.gz"
-	READS2="/media/cc/B/Josemi/TTseq_Feb2022/Reads_project_MG10/MG10_RNA_DRB-4sU/MG10_RNA/*S"$NUM"_R2_001.fastq.gz"
+	READS1="/path/4suDRB/*S"$NUM"_R1_001.fastq.gz"
+	READS2="/path/4suDRB/*S"$NUM"_R2_001.fastq.gz"
 
-	SALIDA="/media/cc/B/Josemi/TTseq_Feb2022/Alignments/MG10_RNA_DRB-4sU/MG9-"$NUM".sam"
+	SALIDA="/path/4suDRB/Alignments/MG9-"$NUM".sam"
 
-	bowtie2 -x /home/cc/JoseMiguel/Genome_files/mm10/Bowtie_index -1 $READS1 -2 $READS2 -S $SALIDA -N 1
+	bowtie2 -x /path/Genome_files/mm10/Bowtie_index -1 $READS1 -2 $READS2 -S $SALIDA -N 1
 done
 wait

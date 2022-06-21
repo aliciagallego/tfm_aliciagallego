46365317299e600f36d7bab58c163396154c2bd1
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-21 18:22:16 +0200

    Update 1_BAMtoSAM.sh

--- 4sUDRBseq/Elongation_Rates_Fuchs/1_BAMtoSAM.sh
+++ 4sUDRBseq/Elongation_Rates_Fuchs/1_BAMtoSAM.sh
@@ -2,12 +2,11 @@
 
 # This script converts BAM files to SAM 
 
-for file in /media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/2_Sorting/Pull/*bam
+for file in /path/4suDRB/Pull/*bam
 do
-	
 	NAMEE=$(basename $file)
 	NAME=${NAMEE%.*}
-	SALIDA="/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/2_Sorting/Pull/Pull_sam/"$NAME".sam"
+	SALIDA="/path/4suDRB/Pull/"$NAME".sam"
     	echo $file
     	samtools view -h $file > $SALIDA
 done

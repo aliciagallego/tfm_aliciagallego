e7817f8bb7adacdc8b062b5fd3a2983aba11e361
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-21 20:11:36 +0200

    Update 2_bedtools_intersect_Cao_RefSeqList.sh

--- H1_Cao_data/2_bedtools_intersect_Cao_RefSeqList.sh
+++ H1_Cao_data/2_bedtools_intersect_Cao_RefSeqList.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-AFILE=/media/cc/B/Alicia/H1_Cao/H1_Cao_output/1_bedtools_intersect/Input_genes_RefSeq_Long_List_2KbTSS.bed
-BAMS=/media/cc/A/Josemi/Cao_H1/Alignment/Pull/
+AFILE=/path/H1_Cao/Intersect/Input_genes_RefSeq_Long_List_2KbTSS.bed
+BAMS=/path/H1_Cao/Alignment/Pull/
 
 for FILE in $BAMS/*.bam
 do
 
 	NAME=$(basename $FILE | sed 's/_s.*//') 
-       	bedtools intersect -a $AFILE -b $FILE -c > /media/cc/B/Alicia/H1_Cao/H1_Cao_output/1_bedtools_intersect/$NAME'_2kbTSS'.bed
+       	bedtools intersect -a $AFILE -b $FILE -c > path/H1_Cao/Intersect/$NAME'_2kbTSS'.bed
 
 done
-

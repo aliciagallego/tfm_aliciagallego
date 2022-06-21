849b7da286d4cec13be21ff267a77595bcc720d3
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-21 19:17:10 +0200

    Update 2.2_histograms_from_tag_directories_Pull.sh

--- 4sUDRBseq/Meta-gene_profiles/2.2_histograms_from_tag_directories_Pull.sh
+++ 4sUDRBseq/Meta-gene_profiles/2.2_histograms_from_tag_directories_Pull.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Tag directory: is a directory that contains several files describing your data. 
+# The input file for making a tag directory is the output of the mapping process *.sam
+
 for NUM in 11-13 12-14 15-17 16-18
 do
 
-	TAG="/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/4.2_TagDirectories/MG9-"$NUM"_TagDirectory/"
+	TAG="/path/4suDRB/Pull/TagDirectories_Tag/MG9-"$NUM"_TagDirectory/"
 
-	OUT="/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/5.2_Histograms_pull/ghist/MG9-"$NUM"_Histogram"      
+	OUT="/path/4suDRB/Pull/Histograms//MG9-"$NUM"_Histogram"      
 
-	annotatePeaks.pl /media/cc/B/Josemi/TTseq_Feb2022/TTseq_data/RefSeq_LongList_TSS_2Kb50Kb_OK.bed none -size 52000 -hist 20 -ghist -d $TAG > $OUT/"MG9-"$NUM"_output.txt"     	 	
+	annotatePeaks.pl /path/4suDRB/RefSeq_LongList_TSS_2Kb50Kb.bed none -size 52000 -hist 20 -ghist -d $TAG > $OUT/"MG9-"$NUM"_output.txt"     	 	
 
 
 done

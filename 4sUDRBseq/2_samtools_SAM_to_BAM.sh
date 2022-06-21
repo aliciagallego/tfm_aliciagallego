eaeb273474436843f0907fc11ee858ded984f4ec
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-21 18:37:03 +0200

    Update 2_samtools_SAM_to_BAM.sh

--- 4sUDRBseq/2_samtools_SAM_to_BAM.sh
+++ 4sUDRBseq/2_samtools_SAM_to_BAM.sh
@@ -6,8 +6,8 @@ NUMS=$(seq 11 18);
 
 for NUM in $NUMS
 do
-	SAM="/media/cc/B/Josemi/TTseq_Feb2022/Alignments/MG10_RNA_DRB-4sU/MG9-"$NUM".sam"
-	SALIDA="/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/1_SAMtoBAM/MG9-"$NUM".bam"
+	SAM="/path/4sUDRB/MG9-"$NUM".sam"
+	SALIDA="/path/4sUDRB/MG9-"$NUM".bam"
 
 	samtools view -S -b $SAM > $SALIDA
 done

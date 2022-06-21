18b254851c68419f0ae2874ebdb0f45e19e54772
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-21 18:43:36 +0200

    Update 7.2_conv_BAM_to_BW_Pull_normalize.sh

--- 4sUDRBseq/7.2_conv_BAM_to_BW_Pull_normalize.sh
+++ 4sUDRBseq/7.2_conv_BAM_to_BW_Pull_normalize.sh
@@ -2,19 +2,18 @@
 
 # This script generates BigWig files from bam files 
 # -bs: size of the bins, in bases, for the output of the bigwig file (default: 50)
+# --normalizeUsing RPKM  normalization is used
 
 for NUM in TKO0 TKO5 WT0 WT5
 do
-        BAM="/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/2_Sorting/Pull/"$NUM"_*.bam"
+        BAM="/path/4suDRB/"$NUM"_*.bam"
 
 	FILE=$(basename $BAM)
 	NAME=${FILE%.*}
 
-	OUTFW="/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/2_Sorting/Pull/"$NAME"_FW.bw"
-	OUTRV="/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Visualize_alignments/MG10_RNA_DRB-4sU/2_Sorting/Pull/"$NAME"_RV.bw"
+	OUTFW="/path/4suDRB/"$NAME"_FW.bw"
+	OUTRV="/path/4suDRB/"$NAME"_RV.bw"
 
-	#samtools index $BAM
-	#bamCoverage -bs 1 -b $BAM -o $OUTPUT
 	bamCoverage -bs 1 -b $BAM --normalizeUsing RPKM --filterRNAstrand forward -o $OUTFW
 	bamCoverage -bs 1 -b $BAM --normalizeUsing RPKM --filterRNAstrand reverse -o $OUTRV
 done

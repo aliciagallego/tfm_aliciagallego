f074d65d0d428d37af67d88d7ddde305b8b47b0f
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-21 17:33:40 +0200

    Update Matlab_script_Pull.m

--- 4sUDRBseq/Elongation_Rates_Fuchs/Matlab_scripts/Matlab_script_Pull.m
+++ 4sUDRBseq/Elongation_Rates_Fuchs/Matlab_scripts/Matlab_script_Pull.m
@@ -1,48 +1,49 @@
-% cd /media/cc/B/Josemi/TTseq_Feb2022/TTseq_scripts/Elongation_Rates_Fuchs/matlab
+% cd /path/Elongation_Rates/matlab
 % Open writing file and print the header
-outfid = fopen('/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Elongation_rate_3/1.1_Rate_calculation/Elongation_rate_5min_20220425_20Kb_size_Pull.txt', 'wt');
+outfid = fopen('/path/Elongation_rate_Pull_output.txt', 'wt');
 fprintf(outfid, "Gene_name\tWT_Pull\tTK0_Pull\n");
 
-% WT
+% WT Pull of the replicates
 all_chr_WT1 = cell(21,2);
 
-% 0 min sample
+% 0 min
 for i = 1:21
-    fid = fopen(strcat('/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Elongation_rate_3/1.1_Rate_calculation/WT0_MG9-11-13/profile_',num2str(i)));
+    fid = fopen(strcat('/path/WT0_MG9-11-13/profile_',num2str(i)));
     raw_data = sparse(fscanf(fid, '%d'));
     fclose(fid);
     all_chr_WT1{i,1} = bin_profile(raw_data,100);
 end
 
-% 5 min sample
+% 5 min
 for i = 1:21
-    fid = fopen(strcat('/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Elongation_rate_3/1.1_Rate_calculation/WT5_MG9-12-14/profile_',num2str(i)));
+    fid = fopen(strcat('/path/WT5_MG9-12-14/profile_',num2str(i)));
     raw_data = sparse(fscanf(fid, '%d'));
     fclose(fid);
     all_chr_WT1{i,2} = bin_profile(raw_data,100);
 end
 
-% H1-TKO
+% H1-TKO Pull of the replicates
 all_chr_TKO1 = cell(21,2);
 
-% 0 min sample
+% 0 min
 for i = 1:21
-    fid = fopen(strcat('/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Elongation_rate_3/1.1_Rate_calculation/TKO0_MG9-15-17/profile_',num2str(i)));
+    fid = fopen(strcat('/path/TKO0_MG9-15-17/profile_',num2str(i)));
     raw_data = sparse(fscanf(fid, '%d'));
     fclose(fid);
     all_chr_TKO1{i,1} = bin_profile(raw_data,100);
 end
 
-% 5 min sample
+% 5 min
 for i = 1:21
-    fid = fopen(strcat('/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Elongation_rate_3/1.1_Rate_calculation/TKO5_MG9-16-18/profile_',num2str(i)));
+    fid = fopen(strcat('/path/TKO5_MG9-16-18/profile_',num2str(i)));
     raw_data = sparse(fscanf(fid, '%d'));
     fclose(fid);
     all_chr_TKO1{i,2} = bin_profile(raw_data,100);
 end
 
+% Introduce information of the input gene list
 % For each gene in the Input_genes.txt file
-mygenes = tdfread('/media/cc/B/Josemi/TTseq_Feb2022/TTseq_output/Elongation_rate_2/4_Input_genes/Input_genes_20Kb.txt', "\t");
+mygenes = tdfread('/path/Input_genes/Input_genes_20Kb.txt', "\t");
 tablesize = size(mygenes.Name, 1);
 
 i=1;

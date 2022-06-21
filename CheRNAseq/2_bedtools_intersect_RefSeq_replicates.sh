5ad6ff4be6cedc624ac2692192258141f94fd4b1
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-21 20:00:43 +0200

    Update 2_bedtools_intersect_RefSeq_replicates.sh

--- CheRNAseq/2_bedtools_intersect_RefSeq_replicates.sh
+++ CheRNAseq/2_bedtools_intersect_RefSeq_replicates.sh
@@ -5,7 +5,7 @@ for NUM in TKO_I TKO_II TKO_III WT_I WT_II WT_III
 do
 	BAM="/path/cheRNA/Alignments/"$NUM".bam"
 	BED=/path/Genome_files/RefSeq_genes.bed
-	SALIDA="path/cheRNA/Intersect_RefSeq/"$NUM"_RefSeq_intersect.bed"
+	SALIDA="/path/cheRNA/Intersect_RefSeq/"$NUM"_RefSeq_intersect.bed"
 
 	bedtools intersect -a $BED -b $BAM -c -s > $SALIDA
 done

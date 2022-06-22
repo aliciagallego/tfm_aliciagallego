e6f9b79326ea98177e6f649446276ec338a0f80e
aliciagallego <99732403+aliciagallego@users.noreply.github.com>
2022-06-22 11:28:17 +0200

    Update 2.2_bedtools_intersect_RefSeq_BAM_promotor.sh

--- RNAPII_ChIPseq/2.2_bedtools_intersect_RefSeq_BAM_promotor.sh
+++ RNAPII_ChIPseq/2.2_bedtools_intersect_RefSeq_BAM_promotor.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-AFILE=/media/cc/A/Alicia/NGS/RNApolII_3/RNApolII3_output/Gene_selection_RefSeq/Input_genes_RefSeq_Long_List_Promoters.bed
+AFILE=/path/RNAPII/Gene_selection_RefSeq/Input_genes_RefSeq_Long_List_Promoters.bed
+BAM=/path/RNAPII/Alignment
 
-NEW=/media/cc/A/Alicia/NGS/RNApolII/RNApolII_data/Archivos_cluster/New_seq
-OLD=/media/cc/A/Alicia/NGS/RNApolII/RNApolII_data/Archivos_cluster/Old_seq
-
-for FILE in $NEW/*.bam
-do
-
-	NAME=$(basename $FILE | sed 's/_S.*//') 
-       	bedtools intersect -c -wa -a $AFILE -b $FILE > /media/cc/A/Alicia/NGS/RNApolII_3/RNApolII3_output/Intersect_RefSeq_BAM/1_Intersect_bedtools/Promoters/$NAME'_wa_PR_500bp'.bed
-
-done
-
-for FILE in $OLD/*.bam
+for FILE in $BAM/*.bam
 do
 	NAME=$(basename $FILE | sed 's/_R.*//')
-       	bedtools intersect -c -wa -a $AFILE -b $FILE  > /media/cc/A/Alicia/NGS/RNApolII_3/RNApolII3_output/Intersect_RefSeq_BAM/1_Intersect_bedtools/Promoters/$NAME'_wa_PR_500bp'.bed
+       	bedtools intersect -c -wa -a $AFILE -b $FILE  > /path/RNAPII/Intersect_bedtools/Promoters/$NAME'_wa_PR_500bp'.bed
 
 done
-
-
